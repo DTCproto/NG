ece6fd99338e422f2182c21aa49bc95e53c32b75
DTCproto <61973955+DTCproto@users.noreply.github.com>
2025-04-26 01:32:58 +0800

    update

--- ext/nouse.go
+++ ext/nouse.go
@@ -4,5 +4,7 @@ import (
 	_ "google.golang.org/genproto/googleapis/type/color"
 )
 
+// Error: /home/runner/go/pkg/mod/google.golang.org/grpc@v1.72.0/status/status.go:35:2:
+// ambiguous import: found package google.golang.org/genproto/googleapis/rpc/status in multiple modules:
 func init() {
 }
